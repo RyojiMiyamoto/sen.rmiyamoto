8fea349de73bbe3d1ebc7b66697c91378f345eff
RyojiMiyamoto <sen.miyamotoryoji@gmail.com>
2016-08-25 06:09:53 +0000

    アクションイベントの追加

---
+++ app/action/Editevent/Newevent.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *  Editevent/Newevent.php
+ *
+ *  @author     {$author}
+ *  @package    Sample
+ */
+
+/**
+ *  editevent_newevent Form implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sample
+ */
+class Sample_Form_EditeventNewevent extends Sample_ActionForm
+{
+    /**
+     *  @access protected
+     *  @var    array   form definition.
+     */
+    public $form = array(
+       /*
+        *  TODO: Write form definition which this action uses.
+        *  @see http://ethna.jp/ethna-document-dev_guide-form.html
+        *
+        *  Example(You can omit all elements except for "type" one) :
+        *
+        *  'sample' => array(
+        *      // Form definition
+        *      'type'        => VAR_TYPE_INT,    // Input type
+        *      'form_type'   => FORM_TYPE_TEXT,  // Form type
+        *      'name'        => 'Sample',        // Display name
+        *
+        *      //  Validator (executes Validator by written order.)
+        *      'required'    => true,            // Required Option(true/false)
+        *      'min'         => null,            // Minimum value
+        *      'max'         => null,            // Maximum value
+        *      'regexp'      => null,            // String by Regexp
+        *
+        *      //  Filter
+        *      'filter'      => 'sample',        // Optional Input filter to convert input
+        *      'custom'      => null,            // Optional method name which
+        *                                        // is defined in this(parent) class.
+        *  ),
+        */
+    );
+
+    /**
+     *  Form input value convert filter : sample
+     *
+     *  @access protected
+     *  @param  mixed   $value  Form Input Value
+     *  @return mixed           Converted result.
+     */
+    /*
+    protected function _filter_sample($value)
+    {
+        //  convert to upper case.
+        return strtoupper($value);
+    }
+    */
+}
+
+/**
+ *  editevent_newevent action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sample
+ */
+class Sample_Action_EditeventNewevent extends Sample_ActionClass
+{
+    /**
+     *  preprocess of editevent_newevent Action.
+     *
+     *  @access public
+     *  @return string    forward name(null: success.
+     *                                false: in case you want to exit.)
+     */
+    public function prepare()
+    {
+        /**
+        if ($this->af->validate() > 0) {
+            // forward to error view (this is sample)
+            return 'error';
+        }
+        $sample = $this->af->get('sample');
+        */
+        return null;
+    }
+
+    /**
+     *  editevent_newevent action implementation.
+     *
+     *  @access public
+     *  @return string  forward name.
+     */
+    public function perform()
+    {
+        return 'editevent_newevent';
+    }
+}
