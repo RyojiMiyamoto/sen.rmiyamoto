22377c8910b2969322365e6de6582cfc2ff39dac
RyojiMiyamoto <sen.miyamotoryoji@gmail.com>
2016-08-25 06:06:55 +0000

    イベント編集画面の生成

---
+++ app/view/Editevent.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Editevent.php
+ *
+ *  @author     {$author}
+ *  @package    Sample
+ */
+
+/**
+ *  editevent view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Sample
+ */
+class Sample_View_Editevent extends Sample_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
+
