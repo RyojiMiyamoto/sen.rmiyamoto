56aa7b54eef6e3925d4284d5e0f8f7e73354466e
RyojiMiyamoto <sen.miyamotoryoji@gmail.com>
2016-09-07 06:50:25 +0000

    認証キー用のランダム英数字文字列を

--- app/Sample_UserManager.php
+++ app/Sample_UserManager.php
@@ -34,6 +34,20 @@ class Sample_UserManager
 		return null;
 	}
 
+	/**
+	* ランダム文字列生成 (英数字)
+	* $length: 生成する文字数
+	*/
+	function makeRandStr($length) {
+		$str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z"'));
+	
+		for ($i = 0; $i < $length; $i++) {
+			$r_str .= $str[rand(0, count($str)-1)];
+		}
+	
+		return $r_str;
+	}
+
 	// old
 	/*
 	public function auth($mailaddress, $password)
